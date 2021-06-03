ea007c90bf212ed845adb3a121dbcfe97b076ac9
Kanchan Chauhan <kanchan827@gmail.com>
2021-06-02 23:46:33 -0400

    hidden search widget

--- progress-bar/script.js
+++ progress-bar/script.js
@@ -40,7 +40,7 @@ function update() {
   if(currentActive === 1) {
     prev.disabled = true;
   } else if (currentActive === circles.length) {
-    next.disabled === true;
+    next.disabled = true;
   } else {
     prev.disabled = false;
     next.disabled = false;

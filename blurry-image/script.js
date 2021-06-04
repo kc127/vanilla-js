1aa912dc1446dac7adc4a03ff22a81ea0c5fc2b9
Kanchan Chauhan <kanchan827@gmail.com>
2021-06-03 22:03:32 -0400

    blurry image opacity

--- blurry-image/script.js
+++ blurry-image/script.js
@@ -5,6 +5,10 @@ let load = 0;
 
 let int = setInterval(blurring, 30);
 
+function scale (number, inMin, inMax, outMin, outMax) {
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
 function blurring () {
   load++;
 
@@ -13,6 +17,7 @@ function blurring () {
   }
 
   text.innerText = `${load}%`
-  text.style.opacity = 
+  text.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 

c9512a236e2ce069b081b36f8f607b214909ecd5
Kanchan Chauhan <kanchan827@gmail.com>
2021-06-03 22:04:16 -0400

    add scale function

--- blurry-image/script.js
+++ blurry-image/script.js
@@ -21,3 +21,4 @@ function blurring () {
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
+/* https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers*/
